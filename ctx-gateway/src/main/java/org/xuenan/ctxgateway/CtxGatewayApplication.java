5a5f9c6410bf21d53d1f156cbc805785bfecbaa7
changyanan <82404677@QQ.COM>
2019-03-02 01:05:09 +0800

    t

---
+++ ctx-gateway/src/main/java/org/xuenan/ctxgateway/CtxGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.xuenan.ctxgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtxGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtxGatewayApplication.class, args);
+	}
+
+}
