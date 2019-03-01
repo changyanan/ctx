2de35241a7463dc3244209e530d3c15cfc733f6f
changyanan <changyanan@globalegrow.com>
2019-03-01 17:08:16 +0800

    添加配置问题文件

---
+++ ctx-register/src/main/java/org/xuenan/ctxregister/CtxRegisterApplication.java
@@ -0,0 +1,13 @@
+package org.xuenan.ctxregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtxRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtxRegisterApplication.class, args);
+    }
+
+}
