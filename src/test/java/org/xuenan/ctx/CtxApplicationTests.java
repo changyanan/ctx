bc9fa68f441743915b53cd71c3768bf0933a586f
changyanan <changyanan@globalegrow.com>
2019-03-01 11:04:41 +0800

    Initial commit

---
+++ src/test/java/org/xuenan/ctx/CtxApplicationTests.java
@@ -0,0 +1,16 @@
+package org.xuenan.ctx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CtxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
